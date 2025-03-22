754d41cc5e565f419ee5f03f58ce12929fe486fb
Luis Alburez <alburezluis@gmail.com>
2025-03-22 11:07:24 -0600

    commit para subir los archivos al repositorio

---
+++ script.js
@@ -0,0 +1,27 @@
+/*Escriba un for...of  que itere a travÃ©s de nuestra pokemonListmatriz.
+
+Dentro del bloque del for...of bucle, use console.log() una interpolaciÃ³n 
+de cadenas como se modelÃ³ anteriormente para registrar cada elemento de la 
+matriz pokemon  dentro de la cadena. 'You caught a 'X'!' 
+Por ejemplo, la primera iteraciÃ³n del bucle deberÃ­a imprimir 
+'Â¡Atrapaste un Pikachu!' en la consola.
+
+Uno de los elementos, 'Yoshi', no es un PokÃ©mon. 
+Si encuentras 'Yoshi', usa continue para omitir este elemento antes de que 
+se registre en la consola.
+*/
+const pokemonList = ["Pikachu", "Charizard", "Squirtle", "Yoshi", "Snorlax"];
+
+/*
+for (const pokemon of pokemonList) {
+  pokemon === "Yoshi" ? console.log() : console.log(`Atrapaste un ${pokemon}`);
+}
+ACA LO HICE CON EL  MÉTODO ABREVIADO, EL CUÁL SE VE MEJOR PERO NO PUDE USAR continue CON ESE MÉTODO  
+*/
+
+for (const pokemon of pokemonList) {
+  if (pokemon === "Yoshi") {
+    continue;
+  }
+  console.log(`Atrapaste un ${pokemon}`);
+}
